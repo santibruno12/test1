3e735a1a67694da1df3018519d6430e74d91ee9c
santiago.bruno@telerecargas.com.ar <santiago.bruno@telerecargas.com.ar>
2023-09-13 17:54:22 -0300

    Modified 16 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjUzNTky/spectral.js?branch_slug=main&token=670578b6-145e-402f-a3fc-0c5cf10524c7";
+export default { extends: ruleset };
